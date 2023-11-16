4d175725a14b9e121797fec424bc8bd7ce585bae
雷平 <leip1142@aresoft.cn>
2023-11-16 22:08:31 +0800

    建立初始页面

---
+++ pages/customization/customization.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <button>定制</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ pages/customizationStart/customizationStart.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <button @click="goCustomization">定制元素</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    goCustomization() {
+      uni.navigateTo({
+        url: "/pages/customization/customization",
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
